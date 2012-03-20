244cc622978cb9e9a9cf4dc308b02c8e04c9a9f5
Hatem Mahmoud <hatem@expressionlab.com>
2012-03-20 17:07:13 +0200

    rails g scaffold article title:string body:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320134948) do
+ActiveRecord::Schema.define(:version => 20120320150338) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tr8n_glossary", :force => true do |t|
     t.string   "keyword"
